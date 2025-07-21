fc4ac713305ca60866319a952f30561f78cda065
ethanlchristensen <ethanlchristensen@outlook.com>
2025-07-20 21:45:34 -0500

    ASDF

--- bot/commands/echo_command.py
+++ bot/commands/echo_command.py
@@ -1,5 +1,4 @@
 import discord
-
 from discord import app_commands
 
 from bot.utils.decarators.command_logging import log_command_usage
@@ -10,9 +9,18 @@ class EchoCommand(app_commands.Command):
         @tree.command(name="echo", description="Send a message as the bot with optional attachment")
         @log_command_usage()
         @is_admin()
-        async def echo(interaction: discord.Interaction, message: str, attachment: discord.Attachment = None):
-            channel = interaction.channel            
+        async def echo(interaction: discord.Interaction, message: str = None, attachment: discord.Attachment = None):
+            channel = interaction.channel
+
+            # Prevent error if both message and attachment are missing
+            if not message and not attachment:
+                await interaction.response.send_message("You must provide either a message or an attachment.", ephemeral=True)
+                return
+
+            # Defer the response to allow time to send a message to the channel
+            await interaction.response.defer(thinking=False)
+
             if attachment:
-                await channel.send(message, file=await attachment.to_file())
+                await channel.send(content=message or "", file=await attachment.to_file())
             else:
-                await channel.send(message)
+                await channel.send(content=message)
