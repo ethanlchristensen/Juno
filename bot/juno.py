916b6a785a8f40ac42be205ee6ed2855dbb6002e
ethanlchristensen <ethanlchristensen@outlook.com>
2025-09-29 21:40:46 -0500

    [update] - erm

--- bot/juno.py
+++ bot/juno.py
@@ -15,19 +15,11 @@ from bot.services import AiServiceFactory, EmbedService, AudioService, MusicQueu
 
 class Juno(commands.Bot):
     def __init__(self, intents):
-        status = (
-            discord.Status.invisible
-            if bool(int(os.getenv("INVISIBLE")))
-            else discord.Status.online
-        )
-        super().__init__(
-            command_prefix="!", intents=intents, status=status, activity=None
-        )
+        status = (discord.Status.invisible if bool(int(os.getenv("INVISIBLE"))) else discord.Status.online)
+        super().__init__(command_prefix="!", intents=intents, status=status, activity=None)
         self.start_time = time.time()
         self.juno_slash = JunoSlash(self.tree)
-        self.ai_service = AiServiceFactory.get_service(
-            provider=os.getenv("PREFERRED_PROVIDER", "openai")
-        )
+        self.ai_service = AiServiceFactory.get_service(provider=os.getenv("PREFERRED_PROVIDER", "openai"))
         self.embed_service = EmbedService()
         self.audio_service = AudioService()
         self.music_queue_service = MusicQueueService(self)
