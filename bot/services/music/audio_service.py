83c2005e18d14e225b6c413a032b3e293de16f8b
Ethan Christensen <ethanlchristensen@outlook.com>
2025-10-06 00:47:38 -0500

    [update] - erm

--- bot/services/music/audio_service.py
+++ bot/services/music/audio_service.py
@@ -12,12 +12,7 @@ from .types import AudioMetaData, AudioSource, FilterPreset
 class AudioService:
     def __init__(self):
         self.ydl_opts = {
-            "format": (
-                "bestaudio[acodec=mp3][protocol!=m3u8]/"
-                "bestaudio[acodec^=mp4a][protocol!=m3u8]/"
-                "bestaudio[protocol!=m3u8]/"
-                "bestaudio/best"
-            ),
+            "format": ("bestaudio[acodec=mp3][protocol!=m3u8]/bestaudio[acodec^=mp4a][protocol!=m3u8]/bestaudio[protocol!=m3u8]/bestaudio/best"),
             "quiet": True,
             "noplaylist": True,
             "extract_flat": False,
